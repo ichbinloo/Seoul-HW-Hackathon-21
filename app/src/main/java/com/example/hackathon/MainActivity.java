dc964b2e784058a6840619db2ce5d4d70a85f4de
Minh <ngminglgdo@gmail.com>
2021-01-01 23:21:28 +0900

    update

--- app/src/main/java/com/example/hackathon/MainActivity.java
+++ app/src/main/java/com/example/hackathon/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.amplifyframework.AmplifyException;
+import com.amplifyframework.api.aws.AWSApiPlugin;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.AWSDataStorePlugin;
 import com.amplifyframework.datastore.generated.model.HeyDoc;
@@ -17,31 +18,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         try {
+            Amplify.addPlugin(new AWSApiPlugin());
             Amplify.addPlugin(new AWSDataStorePlugin());
             Amplify.configure(getApplicationContext());
+
             Log.i("Tutorial", "Initialized Amplify");
-        } catch (AmplifyException e) {
-            Log.e("Tutorial", "Could not initialize Amplify", e);
+        } catch (AmplifyException failure) {
+            Log.e("Tutorial", "Could not initialize Amplify", failure);
         }
-        Amplify.DataStore.query(
-                HeyDoc.class, heydocs -> {
-                    while (heydocs.hasNext()) {
-                        HeyDoc todo = heydocs.next();
-                        Log.i("Tutorial", "==== Todo ====");
-                        Log.i("Tutorial", "Name: " + todo.getName());
 
-                        if (todo.getPriority() != null) {
-                            Log.i("Tutorial", "Priority: " + todo.getPriority().toString());
-                        }
-
-                        if (todo.getDescription() != null) {
-                            Log.i("Tutorial", "Description: " + todo.getDescription());
-                        }
-                    }
-                },
-                failure -> Log.e("Tutorial", "Could not query DataStore", failure)
+        Amplify.DataStore.observe(HeyDoc.class,
+                started -> Log.i("Tutorial", "Observation began."),
+                change -> Log.i("Tutorial", change.item().toString()),
+                failure -> Log.e("Tutorial", "Observation failed.", failure),
+                () -> Log.i("Tutorial", "Observation complete.")
         );
+
     }
 }
